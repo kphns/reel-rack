b0992e2b4926282746f90022a75ee6ca7b43a930
Karsten Huneycutt <kph@alumni.duke.edu>
2016-03-22 02:47:21 -0400

    update for new version of http gem

--- lib/reel/rack/server.rb
+++ lib/reel/rack/server.rb
@@ -117,7 +117,7 @@ module Reel
 
       def status_symbol(status)
         if status.is_a?(Fixnum)
-          Http::Response::STATUS_CODES[status].downcase.gsub(/\s|-/, '_').to_sym
+          HTTP::Response::Status::REASONS[status].downcase.gsub(/\s|-/, '_').to_sym
         else
           status.to_sym
         end
